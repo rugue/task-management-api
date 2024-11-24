51186412e25c89f960ac7d218815b2337e10340c
Osarugue Enehizena <nehirugue@gmail.com>
2024-11-24 01:46:13 +0100

    feat: implement registration and login endpoints in AuthController with user profile retrieval

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UserService } from 'src/user/user.service';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {}
+  @Post('register')
+  async register(@Body() createUserDto: CreateUserDto) {
+    return this.userService.createUser(createUserDto);
+  }
+  @Post('login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+  @UseGuards(AuthGuard('jwt'))
+  @Post('profile')
+  async getProfile(@Request() req) {
+    return req.user;
+  }
+}
